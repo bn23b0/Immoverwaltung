1d9690dc7f56f6b4f0c861c3594b847e39cacc65
bn23b0 <buchnerben@outlook.de>
2026-08-23 14:34:36 +0200

    Delete session.js

--- session.js
+++
@@ -1,5 +0,0 @@
-const KEY = 'imv_token';
-export const getToken = () =>
-  typeof window === 'undefined' ? null : localStorage.getItem(KEY);
-export const setToken = (t) => localStorage.setItem(KEY, t);
-export const clearToken = () => localStorage.removeItem(KEY);
